7de8285f26e89eb40a7a8ea3d4b2045e69749438
sh1lezh <shailesh811cn@gmail.com>
2024-06-24 12:42:43 +0530

    created bot

---
+++ bot.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello! I am your new bot.')
+
+bot.run('MTI1NDY4NTgyMDUzOTg5NTkzMQ.GbqqzC.vwk0d4OqTCu5M0SNOtQAKFCY_ItAf4PxWeY7N0')
