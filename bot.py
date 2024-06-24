8e8dc035ad1e0505ce331773ec5c1736a7cceef7
sh1lezh <shailesh811cn@gmail.com>
2024-06-24 18:16:34 +0530

    removed token

--- bot.py
+++ bot.py
@@ -1,8 +1,5 @@
 import discord
 from discord.ext import commands
-import os
-
-TOKEN = os.getenv('MTI1NDY4NTgyMDUzOTg5NTkzMQ.GbqqzC.vwk0d4OqTCu5M0SNOtQAKFCY_ItAf4PxWeY7N0')
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -16,4 +13,4 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send('Hello! I am your new bot.')
 
-bot.run('TOKEN')
+bot.run('')
